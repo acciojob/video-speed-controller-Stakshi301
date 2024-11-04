f0cc1a4308f4f71d539b2da0fbee32e362098d0d
Stakshi <128694806+Stakshi301@users.noreply.github.com>
2024-11-04 10:49:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ speedSlider.addEventListener('input', () => {
     video.playbackRate = parseFloat(speedSlider.value);
     speedBar.textContent = `${speedSlider.value}×`;
 });
+
+function rewind() {
+  const video = document.getElementById('video');
+  video.currentTime = Math.max(0, video.currentTime - 10); // Rewind by 10 seconds
+}
+
+function skip() {
+  const video = document.getElementById('video');
+  video.currentTime = Math.min(video.duration, video.currentTime + 25); // Skip forward by 25 seconds
+}
